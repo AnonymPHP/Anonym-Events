169a3509c0f567040789ea8bb5ecd4d5e74c1886
vahitserifsaglam1 <vahit.serif119@gmail.com>
2016-12-15 20:18:46 +0500

    updated

--- index.php
+++ index.php
@@ -10,4 +10,17 @@
 
 include 'vendor/autoload.php';
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+$event = new \Anonym\Components\Event\EventDispatcher();
 
+$event->listen('before_save', function($data){
+    var_dump($data);
+});
+
+$event->listen('before_save', function ($data1){
+    var_dump($data1. 'data');
+});
+
+$event->fire('before_save', ['data1']);
